01889cfb1dff1ac9b14741b621ac6f1800e85ee5
Rakesh4574 <e1406567@u.nus.edu>
2026-01-23 15:44:53 +0800

    Add support for echoing user input

--- src/main/java/Groot.java
+++ src/main/java/Groot.java
@@ -1,13 +1,28 @@
+import java.util.Scanner;
+
 public class Groot {
+
     public static void main(String[] args) {
+        new Groot().run();
+    }
+
+    private void run() {
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("""
                 __________________________________________________
-                 Hello! I am Groot. Guardian of your tasks. 
+                 Hello! I am Groot. Guardian of your tasks.
                  What shall we grow today?
                 __________________________________________________
-                 Bye. Until next time!
-                __________________________________________________
                 """);
+
+        while (true) {
+            String input = sc.nextLine();
+            System.out.println("__________________________________________________");
+            System.out.println(input);
+            System.out.println("__________________________________________________");
+        }
     }
 }
+
 
