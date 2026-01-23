65d21a25587f82e1a037c2b529ad5b45b2ab28fd
Rakesh4574 <e1406567@u.nus.edu>
2026-01-23 15:29:04 +0800

    Rename main class to Groot

---
+++ src/main/java/Groot.java
@@ -0,0 +1,13 @@
+public class Groot {
+    public static void main(String[] args) {
+        System.out.println("""
+                __________________________________________________
+                 Hello! I am Groot. Guardian of your tasks. 
+                 What shall we do today?
+                __________________________________________________
+                 Bye. Until next time!
+                __________________________________________________
+                """);
+    }
+}
+
