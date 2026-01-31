6398106b9f3a220acf6986b9125ec488e596c938
Rakesh4574 <e1406567@u.nus.edu>
2026-01-31 21:21:16 +0800

    Add support for echo

--- src/main/java/Groot.java
+++ src/main/java/Groot.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Groot {
+
     public static void main(String[] args) {
+        new Groot().run();
+    }
+
+    private void run() {
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("""
                 __________________________________________________
                  Hello! I am Groot.
@@ -8,6 +17,14 @@ public class Groot {
                  I am Groot. Until next time!
                 __________________________________________________
                 """);
+    }
+
+        while (true) {
+            String input = sc.nextLine();
+            System.out.println("__________________________________________________");
+            System.out.println(" " + input);
+            System.out.println("__________________________________________________");
+        }
     }
 }
 
