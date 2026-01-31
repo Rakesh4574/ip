189860d825fa2b0c18cf0c4d28b20d108e702cea
Rakesh4574 <e1406567@u.nus.edu>
2026-01-31 19:56:20 +0800

    Add greeting

--- src/main/java/Groot.java
+++ src/main/java/Groot.java
@@ -1,12 +1,15 @@
 public class Groot {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("""
+                __________________________________________________
+                 Hello! I am Groot.
+                 What shall we grow today?
+                __________________________________________________
+                 I am Groot. Until next time!
+                __________________________________________________
+                """);
     }
 }
+
 
 
