d10a60255f96471136b979b4fceac2768cfea3b1
Rakesh4574 <e1406567@u.nus.edu>
2026-02-01 14:04:15 +0800

    Update Deadline

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,14 +1,13 @@
 public class Deadline extends Task {
-
-    protected String by;
+    private String by;
 
     public Deadline(String description, String by) {
-        super(description);
+        super(description, TaskType.DEADLINE);
         this.by = by;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return super.toString() + " (by: " + by + ")";
     }
 }
