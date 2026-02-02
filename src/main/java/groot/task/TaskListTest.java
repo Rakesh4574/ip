d8ff4e0dcf7fc69198107670e83c899ac4987b5e
Rakesh4574 <e1406567@u.nus.edu>
2026-02-02 15:22:43 +0800

    Add TaskListTest class

---
+++ src/main/java/groot/task/TaskListTest.java
@@ -0,0 +1,25 @@
+package groot.task;
+
+import groot.GrootException;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class TaskListTest {
+    @Test
+    public void add_todoTask_sizeIncreases() {
+        TaskList tasks = new TaskList();
+        tasks.add(new Todo("read book"));
+        assertEquals(1, tasks.size());
+    }
+
+    @Test
+    public void remove_invalidIndex_exceptionThrown() {
+        try {
+            new TaskList().remove(10);
+            fail(); 
+        } catch (GrootException e) {
+            assertEquals("Invalid task number.", e.getMessage());
+        }
+    }
+}
